e97589b97474bc940f88e7e53159e50e05477c6c
Timo Ischen <timo@Timos-MBP.local>
2019-07-05 20:53:01 +0200

    reset quiz

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -16,18 +17,25 @@ class Result extends StatelessWidget {
     } else {
       resultText = 'Du bist leider scheiße';
     }
-    print(resultScore);
-    print(resultText);
     return resultText;
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz'),
+            onPressed: resetHandler,
+            textColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
